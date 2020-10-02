dab68d4d54d6cd26bbdce8b470a76e86f90e2ef6
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-10-02 15:16:47 +0300

    Добавляє ДЗ-08

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 import gallery from "./gallery-items.js";
 
-// Разбей задание на несколько подзадач:
-
-// Создание и рендер разметки по массиву данных и предоставленному шаблону.
-// Реализация делегирования на галерее ul.js - gallery и получение url большого изображения.
-// Открытие модального окна по клику на элементе галереи.
-// Подмена значения атрибута src элемента img.lightbox__image.
-// Закрытие модального окна по клику на кнопку button[data - action= "close-lightbox"].
-// Очистка значения атрибута src элемента img.lightbox__image.Это необходимо для того,
-//     чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
-
-// Создание и рендер разметки по массиву данных и предоставленному шаблону.
+// 1.Создание и рендер разметки по массиву данных и предоставленному шаблону.
 
 const galleryContainer = document.querySelector(".js-gallery");
 const cardsMarkup = createMarkupGallery(gallery);
@@ -36,16 +26,72 @@ function createMarkupGallery(gallery) {
     .join("");
 }
 
-// Реализация делегирования на галерее ul.js - gallery и получение url большого изображения.
+// 2.Реализация делегирования на галерее ul.js - gallery и получение url большого изображения.
 
 const refs = {
-  lightbox: document.querySelector(".js-lightbox"),
-  imagesForClick: document.querySelector("ul.js-gallery"),
+  lightboxEl: document.querySelector("div.lightbox"),
+  containerImagesForClickEl: document.querySelector("ul.js-gallery"),
+  imgModalEl: document.querySelector(".lightbox__image"),
+  btnCloseModalEl: document.querySelector(`button[data-action="close-lightbox"]`),
+  backdropEl: document.querySelector("div.lightbox__overlay"),
 };
 
-refs.imagesForClick.addEventListener("click", onOpenModal);
+// const selectedImg = evt.target;
+
+refs.containerImagesForClickEl.addEventListener("click", onOpenModal);
+refs.btnCloseModalEl.addEventListener("click", onCloseModal);
+refs.backdropEl.addEventListener("click", onBackdropClick);
 
 function onOpenModal(evt) {
-    console.log("Клікнули на", evt.target);
-    evt.target.url = 
+  evt.preventDefault();
+  window.addEventListener("keydown", onEscClick);
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  evt.target.src = evt.target.dataset.source;
+
+  // 3.Открытие модального окна по клику на элементе галереи.
+  refs.lightboxEl.classList.add("is-open");
+
+  // 4.Подмена значения атрибута src элемента img.lightbox__image.
+  refs.imgModalEl.src = evt.target.dataset.source;
 }
+
+// 5.Закрытие модального окна по клику на кнопку button[data - action= "close-lightbox"].
+// 6.Очистка значения атрибута src элемента img.lightbox__image.Это необходимо для того,
+//     чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+
+function onCloseModal() {
+  window.removeEventListener("keydown", onEscClick);
+
+  refs.lightboxEl.classList.remove("is-open");
+  refs.imgModalEl.src = "";
+}
+
+// 7.Закрытие модального окна по клику на div.lightbox__overlay.
+function onBackdropClick(evt) {
+  if (evt.currentTarget === evt.target) {
+    onCloseModal();
+  }
+}
+
+// 8.Закрытие модального окна по нажатию клавиши ESC.
+function onEscClick(evt) {
+  if (evt.code === "Escape") {
+    onCloseModal();
+  }
+}
+// 9.Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
+// window.addEventListener("keydown", onLeftWriteClick);
+// function onLeftWriteClick(evt) {
+//   if (evt.code !== "ArrowLeft" || "ArrowRight") {
+//     return;
+//   }
+//   evt.elem.nextElementSibling;
+//   console.log(evt.code);
+// }
+//ArrowRight ArrowLeft
+//
+//
+// imgToModalInsert();
